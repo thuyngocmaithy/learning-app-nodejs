157d3c4cbc21082812c61824aafa7a0a4da87c6c
thuyngocmaithy <thuyngocmaithyy@gmail.com>
2024-07-25 22:09:23 +0700

    fix: deploy server

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 // src/index.ts (hoặc file tương tự)
-import express from 'express';
+import express, { Request, Response } from 'express';
 import { connectDB } from './config/connectDB';
 import authRoutes from './routes/authRoutes';
 
@@ -8,6 +8,9 @@ const app = express();
 app.use(express.json()); // Để phân tích cú pháp JSON từ body của yêu cầu
 
 // Sử dụng các tuyến đường xác thực
+app.get('/', (req: Request, res: Response) => {
+    res.send('hello world')
+})
 app.use('/api/auth', authRoutes);
 
 // Kết nối đến cơ sở dữ liệu
