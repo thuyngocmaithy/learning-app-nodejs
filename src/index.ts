0a283fa62baa61f54cbc586745b42a36d23b1e7f
thuyngocmaithy <thuyngocmaithyy@gmail.com>
2024-11-10 19:50:27 +0700

    Thay đổi thiết lập cors

--- src/index.ts
+++ src/index.ts
@@ -73,12 +73,25 @@ setupSockets(io);
 
 
 app.use((req, res, next) => {
-    res.setHeader("Access-Control-Allow-Origin", "*");
-    res.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT, DELETE");
-    res.setHeader("Access-Control-Allow-Headers", "Content-Type");
-    next();
-})
+    res.setHeader(
+        "Access-Control-Allow-Origin",
+        corsOrigin
+    );
+    res.setHeader(
+        "Access-Control-Allow-Methods",
+        "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS,CONNECT,TRACE"
+    );
+    res.setHeader(
+        "Access-Control-Allow-Headers",
+        "Content-Type, Authorization, X-Content-Type-Options, Accept, X-Requested-With, Origin, Access-Control-Request-Method, Access-Control-Request-Headers"
+    );
+    res.setHeader("Access-Control-Allow-Credentials", 'true');
+    res.setHeader("Access-Control-Allow-Private-Network", 'true');
+    //  Firefox caps this at 24 hours (86400 seconds). Chromium (starting in v76) caps at 2 hours (7200 seconds). The default value is 5 seconds.
+    res.setHeader("Access-Control-Max-Age", 7200);
 
+    next();
+});
 app.use(bodyParser.json());
 
 const PORT = process.env.PORT || 5000;
