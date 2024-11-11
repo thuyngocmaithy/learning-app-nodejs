5e3b5ddec4775b55349e338c5051f538c940e0a8
thuyngocmaithy <thuyngocmaithyy@gmail.com>
2024-11-11 20:46:56 +0700

    test

--- src/config/connectDB.ts
+++ src/config/connectDB.ts
@@ -3,10 +3,9 @@ import { AppDataSource } from '../data-source';
 export const connectDB = async (): Promise<void> => {
   try {
     if (!AppDataSource.isInitialized) {
+      console.log('Bắt đầu kết nối đến cơ sở dữ liệu...');
       await AppDataSource.initialize();
     }
-    console.log('Bắt đầu kết nối đến cơ sở dữ liệu...');
-    await AppDataSource.initialize();
     console.log('Cơ sở dữ liệu MySQL được kết nối thành công');
   } catch (error) {
     console.error('Không thể kết nối với cơ sở dữ liệu MySQL:', error);
